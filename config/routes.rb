3d44dd78d785e4c54255a802092cf10feac22e1a
nibrasqureshi <nibrasqureshi1108@gmail.com>
2022-08-31 17:42:24 +0500

    modification in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   devise_for :admins, controllers: { registrations: 'registrations' }
-  #  controllers: {
-  #  sessions: 'admins/sessions'
-
-  # }
-
-  # devise_for :admins
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
