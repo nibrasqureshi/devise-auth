835d1c0db4f70d1e0fc0fd7e7f5208e9b26306f9
nibrasqureshi <nibrasqureshi1108@gmail.com>
2022-08-31 17:43:05 +0500

    applictation controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,5 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
-  # protect_from_forgery with: :exception
-  # before_action :authenticate_admin!, :configure_permitted_parameters, if: :devise_controller?
-
-  # protected
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
-
-  #   devise_parameter_sanitizer.permit(:account_update) do |u|
-  #     u.permit(:first_name, :last_name, :email, :password, :current_password)
-  #   end
-  # end
+  
 end
